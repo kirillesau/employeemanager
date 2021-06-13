bb72999c44ebafdd81044ea834b334473e6a1a0b
Kirill Esau <E=$n9k19k3k4>
2021-06-13 12:39:46 +0200

    Ergänzung der Service Methoden

--- src/main/java/com/example/employeemanager/service/EmployeeService.java
+++ src/main/java/com/example/employeemanager/service/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.example.employeemanager.service;
 
+import com.example.employeemanager.UserNotFoundException;
 import com.example.employeemanager.model.Employee;
 import com.example.employeemanager.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -22,4 +24,20 @@ public class EmployeeService {
     return employeeRepository.save(employee);
   }
 
+  public List<Employee> finAllEmployees() {
+    return employeeRepository.findAll();
+  }
+
+  public Employee updateEmployee(Employee employee) {
+    return employeeRepository.save(employee);
+  }
+
+  public Employee findEmployeeById(Long id) {
+    return employeeRepository.findEmployeeById(id)
+        .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found!"));
+  }
+
+  public void deleteEmployee(Long id) {
+    employeeRepository.deleteEmployeeById(id);
+  }
 }
